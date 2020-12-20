a527972aa6fd589128b41b77d0a4bbf026cb883d
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-12-20 10:44:47 +0000

    hashed password

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const User = require("../models/User");
 const { registerValidation } = require("../utils/validation");
 
@@ -9,10 +10,13 @@ exports.register = async (req, res, next) => {
         .status(400)
         .json({ success: false, message: error.details[0].message });
     }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    console.log("hashpw ==> ", hashedPassword);
     const user = new User({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPassword,
     });
     const saveUser = await user.save(); // mongoose function to save in Db
     res.status(200).json({ success: true, message: { user: user._id } });
