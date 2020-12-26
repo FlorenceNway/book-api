50ea548d4521d1af04844ede39d3663cdb4a6f83
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-12-26 23:47:23 +0000

    login validation

--- utils/validation.js
+++ utils/validation.js
@@ -11,3 +11,12 @@ exports.registerValidation = (data) => {
 
   return schema.validate(data);
 };
+
+exports.loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(20).required(),
+  });
+
+  return schema.validate(data);
+};
